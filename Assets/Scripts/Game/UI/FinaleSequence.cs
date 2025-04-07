999cf539aaaffcdc9131fa2e82443e90d4a5e4c6
klimashin <klimashin.v@gmail.com>
2025-04-07 21:27:58 +0200

    Something

---
+++ Assets/Scripts/Game/UI/FinaleSequence.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Reflex.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+namespace Game.UI
+{
+    public class FinaleSequence : MonoBehaviour
+    {
+        [SerializeField] private List<RectTransform> _cogs;
+        [SerializeField] private Vector2 _speedRange;
+        [SerializeField] private RectTransform _characterPlatform;
+        [SerializeField] private RectTransform _authors;
+        [SerializeField] private LoadingOverlay _loadingOverlay;
+
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private void Start()
+        {
+            AnimationSequence().Forget();
+        }
+
+        private async UniTask AnimationSequence()
+        {
+            await _loadingOverlay.HideOverlay();
+
+            await _characterPlatform.DOMoveY(100f, 5f).ToUniTask();
+
+            foreach (var rectTransform in _cogs)
+            {
+                var speed = Random.Range(_speedRange.x, _speedRange.y);
+                rectTransform.DORotate(new Vector3(0, 0, 360), speed, RotateMode.FastBeyond360)
+                    .SetEase(Ease.Linear)
+                    .SetLoops(-1);
+            }
+            
+            _characterPlatform.DOMoveY(-2000f, 7f);
+
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                await _authors.DOMoveY(-1000f, 30f).ToUniTask(cancellationToken: _cancellationTokenSource.Token);
+            }
+            catch (Exception e)
+            {
+                //ignore
+            }
+
+            await _loadingOverlay.ShowOverlay();
+            var scene = SceneManager.LoadScene(0, new LoadSceneParameters(LoadSceneMode.Single));
+            ReflexSceneManager.PreInstallScene(scene, builder => {});
+        }
+
+        private void Update()
+        {
+            if (_cancellationTokenSource is {IsCancellationRequested: false} && Input.anyKeyDown)
+            {
+                _cancellationTokenSource.Cancel();
+            }
+        }
+    }
+}
